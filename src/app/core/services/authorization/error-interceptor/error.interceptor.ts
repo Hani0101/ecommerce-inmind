b9ae8789dd50c795057f1b919d4e426d9ccf837b
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-24 04:54:40 +0200

    feat(auth): add authorization interceptor and error interceptor
    authorization interceptor handles bearer being sent, and error interceptors handles 404 and 401 errors to relocate users to different pages accordingly

---
+++ src/app/core/services/authorization/error-interceptor/error.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+
+        // Handle 401 Unauthorized
+        if (error.status === 401) {
+          this.router.navigate(['/login']);
+        }
+        // Handle 404 Not Found
+        if (error.status === 404) {
+          this.router.navigate(['/not-found']);
+        }
+
+        return throwError(error);
+      })
+    );
+  }
+}
