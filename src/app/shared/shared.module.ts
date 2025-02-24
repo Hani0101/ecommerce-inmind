8eb07a05f6d268e6df25dcb83d5d99ef2473aa94
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-24 04:16:44 +0200

    chore(module): update shared module to include product card and truncate

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    
+    ProductCardComponent,
+    TruncatePipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ButtonComponent,
-    
+    ProductCardComponent,
+    TruncatePipe
   ]
 })
 export class SharedModule {}
