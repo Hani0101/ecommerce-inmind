62fa37a2a2d76e1d2da1c7396b5f08170941709e
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-24 04:41:01 +0200

    chore(auth): add authentication api url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const enviromnent = {
     salesProductAPI:'https://fakestoreapi.com/products?limit=15',
-    categoryAPI:'https://fakestoreapi.com/products/categories'
+    categoryAPI:'https://fakestoreapi.com/products/categories',
+    authApi:'https://dummyjson.com/auth/login',
 }
