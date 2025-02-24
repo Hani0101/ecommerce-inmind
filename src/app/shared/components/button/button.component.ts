f6b3ab224f76fbbd0f6039b6223d16687a8804fc
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-24 03:39:15 +0200

    feat(button): add shared button with custom styles

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: false,
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() text: string = "";
+  @Input() styles: { [key: string]: string } = {}; 
+}
