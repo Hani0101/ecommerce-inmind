c43b9a699ea398683975f98605415852ca01fd5d
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-24 03:36:35 +0200

    chore(module): update app.module.ts
    added angular material, routing module, app components, forms, and core module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule, 
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    CoreModule,
+
+  ],
+  providers: [
+    provideAnimationsAsync('noop'),
+    provideAnimationsAsync(),
+  ],
+  bootstrap: [AppComponent]
+})
 export class AppModule { }
