e686bffeff4f9371707be2df0ad8a858d3fb568e
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-25 03:08:25 +0200

    feat: add services and components to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,14 +6,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { AppShellComponent } from './app-shell/app-shell.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { FeaturesModule } from '../features/features.module';
 import { SharedModule } from '../shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/authorization/authorization-interceptor/authorization.interceptor';
+import { AuthenticationService } from './services/authentication/authentication-service/authentication.service';
 import { RouterModule } from '@angular/router';
+import { ErrorInterceptor } from './services/authorization/error-interceptor/error.interceptor';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 
 @NgModule({
-  declarations: [AppShellComponent,NavbarComponent, ], 
-  imports: [CommonModule,
+  declarations: [AppShellComponent, LogInComponent, SignUpComponent, NavbarComponent, NotFoundComponent], 
+  imports: [CommonModule, 
     ReactiveFormsModule,
     FeaturesModule,
     SharedModule,
@@ -21,9 +29,29 @@ import { RouterModule } from '@angular/router';
     MatInputModule,     
     MatIconModule,
     RouterModule.forRoot([
+      { path: 'log-in', component: LogInComponent },
+      { path: 'sign-up', component: SignUpComponent },
+      { path: 'not-found', component: NotFoundComponent },
     ]),
   ],        
    providers: [
+    AuthenticationService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic'
+      }
+    }
   ],
   exports: [AppShellComponent, NavbarComponent], 
 })
