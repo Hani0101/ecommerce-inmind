8929be9efb4a26c9139886bca76bfba4896f7c48
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-02 17:12:40 +0200

    feat(prduct-cart): add a method to dispatch products added to cart

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../../models/product';
-
+import { Store } from '@ngrx/store';
+import { addToCart } from '../../../state/cart.actions';
+import { inject } from '@angular/core';
 @Component({
   selector: 'app-product-card',
-  standalone: false,
   templateUrl: './product-card.component.html',
+  standalone: false,
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
-  @Input() product: IProduct = {} as IProduct; 
+  @Output() add = new EventEmitter<number>();
+  @Input() product: IProduct = {} as IProduct;
+
+  private store$ = inject(Store);
+
+  addToCart(product: IProduct) {
+    console.log("product: ", product);
+    this.store$.dispatch(addToCart({ product }));
+  }
 }
