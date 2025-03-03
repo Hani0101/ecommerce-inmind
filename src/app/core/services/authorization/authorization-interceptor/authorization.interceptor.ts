fde4c3d58ce8c25a2b9215a792c6653a86748808
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-03 20:43:59 +0200

    chore(jwt-header): disable jwt header enterceptor being sent to the local backend since its not required

--- src/app/core/services/authorization/authorization-interceptor/authorization.interceptor.ts
+++ src/app/core/services/authorization/authorization-interceptor/authorization.interceptor.ts
@@ -2,25 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../../authentication/authentication-service/authentication.service';
-
+import { enviromnent } from '../../../../../environments/environments';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthenticationService) {}
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.authService.getAuthToken();
+    const isBackendRequest = req.url.includes(enviromnent.cartAPI);
 
-    if (authToken) {
+    if (!isBackendRequest && authToken) {  //Disable backend jwt token enterceptor because the api does not require a jwt token
       const cloned = req.clone({
         setHeaders: {
           Authorization: `Bearer ${authToken}`
         }
       });
-      console.log("Authntication interceptor jwt: ", authToken);
+      console.log("Authentication interceptor jwt: ", authToken);
       return next.handle(cloned);
-    } else {
-      return next.handle(req);
     }
+
+    return next.handle(req);
   }
 }
