dd3ad522498462482df450854a38a065bfcf817f
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-03 20:46:28 +0200

    feat(cart-items): add new add item to cart method and update get cart to return new interface

--- src/app/features/services/cart/cart.service.ts
+++ src/app/features/services/cart/cart.service.ts
@@ -1,17 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IProduct } from '../../../shared/models/product';
-import { enviromnent } from '../../../../environments/environments';
+import { ICart } from '../../../shared/models/cart.model';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private CartApiUrl = enviromnent.CartAPI;
+  private apiUrl = 'http://localhost:5058/api/cart'; 
 
   constructor(private http: HttpClient) {}
-
-  getCart(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(`${this.CartApiUrl}`); 
+  getCart(userId: number): Observable<ICart> {
+    return this.http.get<ICart | null>(`${this.apiUrl}/${userId}`).pipe(
+      map(cart => {
+        if (cart && cart.items && '$values' in cart.items) {
+          return { 
+            ...cart, 
+            cartItems: cart.items.$values 
+          };
+        }
+        return {
+          id: 0,
+          userId,
+          createdAt: '',
+          updatedAt: null,
+          items: { $values: [] },
+          cartItems: [] 
+        };
+      })
+    );
   }
+  
+  
+  
+
+  addItemToCart(userId: number, cartId: number, productId: number, quantity: number): Observable<ICart> {
+    const body = { userId, productId, quantity };
+    console.log("cartId", cartId)
+    return this.http.post<ICart>(`${this.apiUrl}/${cartId}/items`, body);
+  }
+  
+  
 }
