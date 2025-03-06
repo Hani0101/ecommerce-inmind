93be1fe7fb6a3747473cc33b8d8a000ef76c7186
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-06 20:38:09 +0200

    chore(shared-button-styles): apply themed shared button styles and delete old styles

--- src/app/features/components/hero/hero.component.ts
+++ src/app/features/components/hero/hero.component.ts
@@ -1,42 +1,14 @@
-import { Component, HostListener  } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-hero',
   standalone: false,
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-  ButtonText = 'Shop Now';
-  btnStyles: { [key: string]: string } = {};
-
-  constructor() {
-    this.updateStyles(window.innerWidth); // Initial call
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.updateStyles(event.target.innerWidth);
-  }
-
-  private updateStyles(width: number) {
-    if (width > 1024) {
-      // Desktop styles
-      this.btnStyles = {
-        'color': '$text-color-primary',
-      };
-    } else if (width > 768) {
-      // Tablet styles
-      this.btnStyles = {
-        'font-size': '0.95rem',
-        'width': '120px',
-      };
-    } else {
-      // Mobile styles
-      this.btnStyles = {
-        'font-size': '0.6rem',
-        'width': '80px',
-        'height': '20px',
-      };
-    }
+  constructor(private router: Router) {}
+  NavigateToCategory(){
+    this.router.navigate(['/category']);
   }
 }
