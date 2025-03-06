33ddcecb0880d935f1bf038011afa1d00324c3bd
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-06 20:06:21 +0200

    feat(router): add category page, landing page, search page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppShellComponent } from '../app/core/app-shell/app-shell.component';
 import { LogInComponent } from './core/pages/log-in/log-in.component';
 import { SignUpComponent } from './core/pages/sign-up/sign-up.component';
 import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 import { CartComponent } from './features/pages/cart/cart.component';
-
+import { CategoryComponent } from './features/pages/category/category.component';
+import { LandingPageComponent } from './features/pages/landing-page/landing-page.component';
+import { SearchResultsComponent } from './features/pages/search-results/search-results.component';
 const routes: Routes = [
   { path: 'log-in', component: LogInComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: '', component: AppShellComponent },  
+  { path: '', component: LandingPageComponent },  
   { path: 'not-found', component: NotFoundComponent },
-  { path: '**', redirectTo: '/not-found' }, 
   { path: 'cart', component: CartComponent },
+  { path: 'category/:name', component: CategoryComponent },
+  { path: 'search', component: SearchResultsComponent },
+  { path: '**', redirectTo: '/not-found' }, 
 
 ];
 
