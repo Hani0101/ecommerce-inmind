081616e919cf3fbb8323df76f913dcc278bc5c51
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-06 20:16:00 +0200

    feat(search-page): add search page

---
+++ src/app/features/pages/search-results/search-results.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product/product.service';
+import { IProduct } from '../../../shared/models/product';
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  standalone: false,
+  styleUrls: ['./search-results.component.scss']
+})
+export class SearchResultsComponent implements OnInit {
+  searchQuery: string = '';
+  products: IProduct[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.searchQuery = params['q'] || '';
+      if (this.searchQuery) {
+        this.searchProducts();
+      }
+    });
+  }
+
+  searchProducts(): void {
+    this.productService.getProductsBySearch(this.searchQuery)
+      .subscribe(data => this.products = data.products);
+  }
+}
