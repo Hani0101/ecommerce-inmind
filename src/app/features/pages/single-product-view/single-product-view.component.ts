3abdf26f75b96eeedea2dcfa4fc47cbab4aa61b1
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-11 03:42:04 +0200

    feat: add pop up to spv

--- src/app/features/pages/single-product-view/single-product-view.component.ts
+++ src/app/features/pages/single-product-view/single-product-view.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@an
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product/product.service';
 import { IProduct } from '../../../shared/models/product';
+import { Store } from '@ngrx/store';
+import { addItemToCart } from '../../../state/cart.actions';
+import { inject } from '@angular/core';
+import { AuthenticationService } from '../../../core/services/authentication/authentication-service/authentication.service';
+import { Router } from '@angular/router';
+import { ICartItem } from '../../../shared/models/cart-item.model';
+
 
 @Component({
   selector: 'app-single-product-view',
@@ -12,14 +19,13 @@ import { IProduct } from '../../../shared/models/product';
 export class SingleProductViewComponent implements OnInit {
   @ViewChild('mainImage') mainImage!: ElementRef;
 
-  product: IProduct | null = null;
+  product!: IProduct;
   selectedImage: string = '';
   isInWishlist: boolean = false;
-
+  showPopup = false;
   constructor(
     private route: ActivatedRoute, 
     private productService: ProductService,
-    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -49,13 +55,31 @@ export class SingleProductViewComponent implements OnInit {
     return Array.from({ length: 5 }, (_, i) => i + 1);
   }
 
-  addToCart() {
-    console.log('Adding to cart:', this.product);
+  private readonly store$ = inject(Store);
+  private readonly authService = inject(AuthenticationService);
+
+  addToCart(product: IProduct) {
+    if(this.product){
+      const userId = this.authService.decodeJwtToken(this.authService.getAccessToken());
+
+      const cartItem: ICartItem = {
+        id: 0, //Default values
+        userId: Number(userId),
+        cartId: 0, //Default values
+        productId: product.id,
+        quantity: 1 //Default values
+      };
+
+      this.store$.dispatch(addItemToCart({ cartItem }));
+
+      this.showPopup = true;
+
+      setTimeout(() => {
+        this.showPopup = false;
+      }, 1500);
+    }
   }
 
-  buyNow() {
-    console.log('Buying now:', this.product);
-  }
 
   toggleWishlist() {
     this.isInWishlist = !this.isInWishlist;
@@ -64,14 +88,12 @@ export class SingleProductViewComponent implements OnInit {
   enableZoom(): void {
     if (this.mainImage) {
       this.mainImage.nativeElement.style.transform = "scale(1.5)";
-      this.cdr.detectChanges(); 
     }
   }
 
   disableZoom(): void {
     if (this.mainImage) {
       this.mainImage.nativeElement.style.transform = "scale(1)";
-      this.cdr.detectChanges(); 
     }
   }
 
@@ -81,7 +103,6 @@ export class SingleProductViewComponent implements OnInit {
       const x = ((event.clientX - left) / width) * 100;
       const y = ((event.clientY - top) / height) * 100;
       this.mainImage.nativeElement.style.transformOrigin = `${x}% ${y}%`;
-      this.cdr.detectChanges(); 
     }
   }
 }
