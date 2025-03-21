4bad9bd324cab1bd4761a4660071621796c377ce
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-03-21 03:40:27 +0200

    fix(cart-checkout): fix cart checkout to get items from database

--- src/app/features/pages/checkout/checkout.component.ts
+++ src/app/features/pages/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { loadCart } from "../../../state/cart.actions";
+import { loadCart, getTotalprice } from "../../../state/cart.actions";
 import { selectCartItems, selectTotalPrice } from '../../../state/cart.selector';
 import { Observable, forkJoin } from 'rxjs';
 import { IProduct } from '../../../shared/models/product'; 
@@ -8,7 +8,7 @@ import { AuthenticationService } from '../../../core/services/authentication/aut
 import { Store } from '@ngrx/store';
 import { map, mergeMap } from 'rxjs/operators';
 import { ProductService } from '../../services/product/product.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-checkout',
   standalone: false,
@@ -23,42 +23,22 @@ export class CheckoutComponent {
   shippingForm: FormGroup;
   paymentForm: FormGroup;
   isProcessing = false;
-
-  cartItems = [
-    {
-      name: "Wireless Headphones",
-      quantity: 1,
-      price: 99.99,
-      image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
-    },
-    {
-      name: "Smart Watch",
-      quantity: 1,
-      price: 199.99,
-      image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
-    }
-  ];
-
+  countries = ["United States", "Canada", "United Kingdom", "Australia", "Germany"];
+  shipping = 5.99;
+  total = 0;
+  subtotal = 0;
   shippingMethods = [
     { id: "standard", name: "Standard Shipping", price: 5.99, time: "5-7 Business Days" },
     { id: "express", name: "Express Shipping", price: 12.99, time: "2-3 Business Days" },
     { id: "overnight", name: "Overnight Delivery", price: 24.99, time: "Next Business Day" }
   ];
-
   paymentMethods = [
     { id: "credit", name: "Credit Card", icon: "https://images.unsplash.com/photo-1542065435-d6bc2eac3377" },
-    { id: "paypal", name: "PayPal", icon: "https://images.unsplash.com/photo-1563013544-824ae1b704d3" },
-    { id: "apple", name: "Apple Pay", icon: "https://images.unsplash.com/photo-1563013544-824ae1b704d3" }
   ];
 
-  countries = ["United States", "Canada", "United Kingdom", "Australia", "Germany"];
+ 
 
-  subtotal = 299.98;
-  tax = 24.99;
-  shipping = 5.99;
-  total = 330.96;
-
-  constructor(private fb: FormBuilder,private authService: AuthenticationService, private store: Store,    private productService: ProductService) {
+  constructor(private fb: FormBuilder,private authService: AuthenticationService, private store: Store, private productService: ProductService, private router: Router) {
     this.shippingForm = this.fb.group({
       fullName: ["", Validators.required],
       email: ["", [Validators.required, Validators.email]],
@@ -84,30 +64,31 @@ export class CheckoutComponent {
     this.loadCartData();
   }
 
-  
-    private loadCartData(): void {
-      const user = this.authService.getUserData();
-      if (!user) {
-        console.error("User not found! Please log in.");
-        return;
-      }
-      const userId = Number(user.id);
-      this.store.dispatch(loadCart({ userId }));
-      this.totalPrice$ = this.store.select(selectTotalPrice);
-      this.cartItems$ = this.store.select(selectCartItems).pipe(
-        mergeMap(cartItems =>
-          forkJoin(cartItems.map(cartItem =>
-            this.productService.getProductById(cartItem.productId).pipe(
-              map(product => ({ product, quantity: cartItem.quantity }))
-            )
-          ))
-        )
-      );
-      this.totalPrice$.subscribe(totalPrice => {
-        this.total = totalPrice + this.tax + this.shipping;
-      });
-    }
+  private loadCartData(): void {
+    this.getPrice();
+    const userId = this.authService.decodeJwtToken(this.authService.getAccessToken());
+    console.log("USER ID FROM CART", userId);
+    this.store.dispatch(loadCart({ userId: Number(userId) }));
+    this.cartItems$ = this.store.select(selectCartItems).pipe(
+      mergeMap(cartItems =>
+        forkJoin(cartItems.map(cartItem =>
+          this.productService.getProductById(cartItem.productId).pipe(
+            map(product => ({ product, quantity: cartItem.quantity }))
+          )
+        ))
+      )
+    );
+  }
 
+  getPrice(){
+    const userId = this.authService.decodeJwtToken(this.authService.getAccessToken());
+    this.totalPrice$ = this.store.select(selectTotalPrice);
+    this.store.dispatch(getTotalprice({  userId:  Number(userId) }));
+    this.totalPrice$.subscribe(totalPrice => {
+      this.subtotal = totalPrice;
+      this.total = totalPrice + this.shipping;
+    });    
+  }
     getDiscountedPrice(price: number, discount: number | null): number {
       if (!price) return 0;
       return discount ? price * (1 - discount / 100) : price;
@@ -140,19 +121,8 @@ export class CheckoutComponent {
     return this.paymentMethods.find(m => m.id === methodId)?.name || "";
   }
 
-  async placeOrder() {
-    this.isProcessing = true;
-    try {
-      // Simulate API call
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      console.log("Order placed successfully");
-      // Handle success
-    } catch (error) {
-      console.error("Error placing order:", error);
-      // Handle error
-    } finally {
-      this.isProcessing = false;
-    }
+  NavigateHome(){
+    this.router.navigate(['/']);
   }
 }
 
